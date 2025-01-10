333f6e477491111cc3660cc3a6e1dbca01013e13
jina ham <hamjina61@gmail.com>
2025-01-10 20:17:31 +0900

    section2

---
+++ section2/springsecsection2/src/main/java/com/example/config/ProjectSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {requests
+                .requestMatchers("/myAccount","myBalance","/myLoans","/myCards").authenticated()
+                .requestMatchers("/notices","/contact","/error").permitAll(); //웹 애플리케이션 내의 모든 요청 보안x
+
+        });
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+}
