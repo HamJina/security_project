72a2c188aca3144e73774282a41fce9a2bc938ef
jina ham <hamjina61@gmail.com>
2025-01-19 21:05:21 +0900

    ãjwt검증 로직 구현

--- section11/springsecsection11/src/main/java/com/example/filter/JWTTokenValidationFilter.java
+++ section11/springsecsection11/src/main/java/com/example/filter/JWTTokenValidationFilter.java
@@ -1,18 +1,53 @@
 package com.example.filter;
 
+import com.example.constants.ApplicationConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 //jwt토큰 유효성 검사 필터
 public class JWTTokenValidationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader(ApplicationConstants.JWT_HEADER);
+        if(null != jwt) {
+            try {
+                Environment env = getEnvironment();
+                if (null != env) {
+                    String secret = env.getProperty(ApplicationConstants.JWT_SECRET_KEY, //토큰생성때 사용한 동일한 secretkey로 유효성 검사
+                            ApplicationConstants.JWT_SECRET_DEFAULT_VALUE);
+                    SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+                    if(null !=secretKey) {
+                        Claims claims = Jwts.parser().verifyWith(secretKey) //jwt토큰 검증하기
+                                .build().parseSignedClaims(jwt).getPayload();
+                        String username = String.valueOf(claims.get("username")); //토큰에서 사용자 정보 가져오기
+                        String authorities = String.valueOf(claims.get("authorities"));
+                        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, //사용자 정보를 토대로 인증객체 생성
+                                AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+                        SecurityContextHolder.getContext().setAuthentication(authentication); //SecurityContext에 저장
+                    }
+                }
 
+            } catch (Exception exception) {
+                throw new BadCredentialsException("Invalid Token received!");
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 
     @Override
